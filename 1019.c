c86e0d021d638db804573bad1e1a94d1305c83ab
Lucas Ronchi <lucascronchi2005@gmail.com>
2026-06-01 13:43:52 -0300

    add ex 1019.c

---
+++ 1019.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main() {
+    int hour, min, sec;
+
+    scanf("%d", &sec);
+
+    min = sec / 60;
+    sec = sec % 60;
+
+    hour = min / 60;
+    min = min % 60;
+
+    printf("%d:%d:%d\n", hour, min, sec);
+
+    return 0;
+}
