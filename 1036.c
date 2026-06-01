bac89989f75b0af18522164da428fec026d2c748
Lucas Ronchi <lucascronchi2005@gmail.com>
2026-06-01 14:32:16 -0300

    add ex 1036.c

---
+++ 1036.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int main() {
+    double a, b, c;
+
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+    if (a == 0) {
+        printf("Impossivel calcular\n");
+        return 0;
+    }
+
+    double delta = b * b - 4 * a * c;
+
+    if (delta < 0) {
+        printf("Impossivel calcular\n");
+        return 0;
+    }
+
+    delta = sqrt(delta);
+    double r1 = (- b + delta) / (2 * a);
+    double r2 = (- b - delta) / (2 * a);
+
+    printf("R1 = %.5f\n", r1);
+    printf("R2 = %.5f\n", r2);
+
+    return 0;
+}
